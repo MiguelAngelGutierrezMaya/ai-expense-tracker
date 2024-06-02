be4a7a72a03e488d876b35b76cd30e7ab59bf2b8
Miguel Angel <gutierrezmayamiguelangel@gmail.com>
2024-06-02 15:34:01 -0500

    Initial Commit

---
+++ AIExpenseTracker/AIExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIExpenseTrackerApp.swift
+//  AIExpenseTracker
+//
+//  Created by Miguel Angel Gutierrez Maya on 2/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AIExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
